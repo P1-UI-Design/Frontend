a0ce59899331b330d6e0862350e7a223e3c24d72
Minato Aqua <zhuocd.zhang@mail.utoronto.ca>
2023-12-08 15:59:41 -0500

    Landing page

--- src/pages/landing/index.jsx
+++ src/pages/landing/index.jsx
@@ -1,10 +1,15 @@
+import React from 'react';
+import './css/main.css';
+import logo from './media/welcome.svg'
+
 function Landing() {
-    return <>
-        <div>
-            <h1>landing Hello, World!</h1>
-            {/* Add more components or content as needed */}
+    return (
+        <div className="main-container">
+            <img src={logo} alt="Logo" className="welcome-logo"/>
+            <h1 className="main-heading">Welcome to our Dog camp!</h1>
+            <a href="/login" className="btn login-button">Become a dog eater</a>
         </div>
-    </>;
+    );
 }
 
 export default Landing;
