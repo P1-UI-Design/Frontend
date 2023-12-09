c6ff69495fc5c0898226206d8a0e8e723ca3e90d
thmcheung <thmarcus.cheung@mail.utoronto.ca>
2023-12-09 14:42:30 -0500

    added applications

---
+++ src/pages/applications/index.jsx
@@ -0,0 +1,15 @@
+import React from "react";
+import ReactDOM from "react-dom";
+
+function Applications() {
+  return (
+    <>
+      <div>
+        <h1>account Hello, World!</h1>
+        {/* Add more components or content as needed */}
+      </div>
+    </>
+  );
+}
+
+export default Applications;
